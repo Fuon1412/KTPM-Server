d77292ddbfbc83c0bbe43494e0bf192b47792eb0
bachhungcb <bachhungcb@gmail.com>
2025-04-01 17:53:58 +0700

    Create Product Model

---
+++ Server/Models/Product/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Server.Models.Product
+{
+    public class ProductModel
+    {
+        /// <example> 12312321 </example>
+        public Guid Id { get; set; }
+        /// <example> Test </example>
+        public required string Name { get; set; }
+        public string? Brand { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+        public string? Image { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
