047cb9fd5fc880a8e27393017699e943801e6b2b
bachhungcb <bachhungcb@gmail.com>
2025-04-01 17:53:38 +0700

    create productExceptions

---
+++ Server/Middlewares/ProductExceptions.cs
@@ -0,0 +1,9 @@
+﻿using Server.Enums.ErrorCodes;
+
+namespace Server.Middlewares
+{
+    public class ProductExceptions(ProductErrorCode errorCode, string message) : Exception(message)
+    {
+        public ProductErrorCode ErrorCode { get; set; } = errorCode;
+    }
+}
