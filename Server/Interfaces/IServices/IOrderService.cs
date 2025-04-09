fc3c9b31cd135774c87b02c562e545e425a85b0f
bachhungcb <bachhungcb@gmail.com>
2025-04-09 23:37:32 +0700

    <feat>: Update IServices

---
+++ Server/Interfaces/IServices/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using Server.DTOs.Order;
+using Server.Models.Order;
+
+namespace Server.Interfaces.IServices
+{
+    public interface IOrderService
+    {
+        Task<OrderModel> CreateOrder(CreateOrderDTO orderInfo);
+        Task UpdateOrder(Guid orderId,
+                         UpdateOrderDTO model
+                         );
+        Task<OrderModel> GetOrderAsync(Guid orderId);
+        Task DeleteOrderAsync(Guid orderId);
+        Task<List<OrderModel>> GetListOrdersAsync();
+    }
+}
